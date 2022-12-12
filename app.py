ecaa337541eb53f0463481861d3c2d5d67cda140
zf81 <111600840+zf81@users.noreply.github.com>
2022-12-12 01:33:17 -0500

    .

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return '<h1>Hello World!</h1>'
+
+@app.route('/page1')
+def page1():
+    return '<h1>Page 1</h1>'
+
+@app.route('/page2')
+def page2():
+    return '<h1>Page 2</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
